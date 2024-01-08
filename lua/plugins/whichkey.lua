9e57b45d0cd2ebdbf64688f9cc3d7fa634e21970
DustinHe <16hjt@pku.edu.cn>
2024-01-08 11:44:33 +0800

    whichkey disable nowait trigger for C-r

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,15 +1,31 @@
 local whichkey = {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 1000
-  end,
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    enabled = true,
+    init = function()
+        vim.o.timeout = true
+        vim.o.timeoutlen = 1000
+    end,
+    opts = {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+        triggers_nowait = {
+            -- marks
+            "`",
+            "'",
+            "g`",
+            "g'",
+            -- registers
+            '"',
+            -- "<c-r>",
+            -- spelling
+            "z=",
+        },
+        disable = {
+            buftypes = { "nofile" }
+        }
+    }
 }
 
 return whichkey
