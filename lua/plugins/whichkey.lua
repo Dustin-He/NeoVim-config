06c04543d74b52f38b9f14e6b22488e590765c08
Dustin-He <16hjt@pku.edu.cn>
2024-07-26 22:39:41 +0800

    change whichkey config

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -1,29 +1,30 @@
 local whichkey = {
     "folke/which-key.nvim",
     lazy = true,
-    -- event = "VeryLazy",
-    enabled = true,
+    event = "VeryLazy",
+    dependencies = {{"echasnovski/mini.icons", version = false}},
+    -- enabled = true,
     cmd = "WhichKey",
-    init = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 1000
-    end,
+    -- init = function()
+    --     vim.o.timeout = true
+    --     vim.o.timeoutlen = 1000
+    -- end,
     opts = {
         -- your configuration comes here
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
-        triggers_nowait = {
-            -- marks
-            "`",
-            "'",
-            "g`",
-            "g'",
-            -- registers
-            '"',
-            -- "<c-r>",
-            -- spelling
-            "z=",
-        },
+        -- triggers_nowait = {
+        --     -- marks
+        --     "`",
+        --     "'",
+        --     "g`",
+        --     "g'",
+        --     -- registers
+        --     '"',
+        --     -- "<c-r>",
+        --     -- spelling
+        --     "z=",
+        -- },
         disable = {
             buftypes = { "nofile" },
             filetypes = {'vim'}
