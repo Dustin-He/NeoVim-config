908eb553207b8bd2a87019790e90da67d3929d74
DustinHe <16hjt@pku.edu.cn>
2026-01-18 01:54:20 +0800

    blink: sync request timeout with minuet

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -150,7 +150,7 @@ local blink = {
                     async = true,
                     -- Should match minuet.config.request_timeout * 1000,
                     -- since minuet.config.request_timeout is in seconds
-                    timeout_ms = 3000,
+                    timeout_ms = 10000,
                     score_offset = 50, -- Gives minuet higher priority among suggestions
                 },
                 buffer = {
