dcde46ff0c34ecc0349b13ba3650b6acdec47118
DustinHe <16hjt@pku.edu.cn>
2026-01-21 02:26:24 +0800

    chagne package path in lualine.lua

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,4 +1,3 @@
-package.path = package.path .. ';' .. '/Users/dustin/.local/share/nvim/lazy/minuet-ai.nvim/lua/?.lua'
 package.path = package.path .. ';' .. '/Users/dustin/.local/share/nvim/lazy/lualine.nvim/lua/?.lua'
 
 -- cool function for progress
