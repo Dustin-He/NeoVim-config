443eed71c4131c57354fe90b9a6f45355ba9dea5
DustinHe <16hjt@pku.edu.cn>
2026-02-05 10:53:48 +0800

    change transparent option for lazygit

--- lua/plugins/tools/lazygit.lua
+++ lua/plugins/tools/lazygit.lua
@@ -6,7 +6,7 @@ local lazygit = {
     },
     cond = (function() return not vim.g.vscode end),
     init = function()
-        vim.g.lazygit_floating_window_winblend = 1 -- transparency of floating window
+        vim.g.lazygit_floating_window_winblend = 0 -- transparency of floating window
         vim.g.lazygit_floating_window_scaling_factor = 0.9
         local opts = { noremap = true, silent = true }
         vim.api.nvim_set_keymap("n", "<leader>g", ":LazyGit<CR>", opts)
